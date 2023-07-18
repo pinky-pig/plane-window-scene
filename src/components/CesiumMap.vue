e3f995e72625ab23baa62093fa5ca4373fdc8307
pinky-pig <2545589993@qq.com>
2023-07-18 16:32:57 +0800

    fix: skybox 修改

--- src/components/CesiumMap.vue
+++ src/components/CesiumMap.vue
@@ -3,6 +3,9 @@ import { onMounted } from 'vue'
 import * as Cesium from 'cesium'
 import { DEFAULT_OPTION } from '~/cesium/params'
 
+// eslint-disable-next-line import/no-named-default
+import { default as SkyBox } from '~/cesium/skybox'
+
 const props = withDefaults(
   defineProps<{
     mapKey?: string
@@ -21,6 +24,7 @@ let viewer: Cesium.Viewer | null
 onMounted(() => {
   initMars3d(props.options)
 })
+
 onUnmounted(() => {
   if (viewer) {
     viewer.destroy()
@@ -35,6 +39,26 @@ function initMars3d(option: any) {
       { ...DEFAULT_OPTION, ...option },
     )
     emit('onload', viewer)
+
+    const blueSkySkybox = new SkyBox({
+      sources: {
+        positiveX: '/skybox/px.png', // 右
+        negativeX: '/skybox/nx.png', // 左
+        positiveY: '/skybox/pz.png', // 上
+        negativeY: '/skybox/nz.png', // 下
+        positiveZ: '/skybox/py.png', // 前
+        negativeZ: '/skybox/ny.png', // 后
+      },
+      nearGround: true,
+    })
+    // 如果相机高度小于2000米，则显示蓝天
+    viewer.scene.postRender.addEventListener(() => {
+      const e = viewer!.camera.position
+      if (Cesium.Cartographic.fromCartesian(e).height < 2000) {
+        viewer!.scene.skyBox = blueSkySkybox
+        viewer!.scene.skyAtmosphere.show = false
+      }
+    })
   }
 }
 </script>
