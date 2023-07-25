1c5e786aa0fe39328b1a0112915d09f691dd35ea
pinky-pig <2545589993@qq.com>
2023-07-25 15:46:47 +0800

    fixed: styles

--- src/components/AnimateBar.vue
+++ src/components/AnimateBar.vue
@@ -16,13 +16,38 @@ const btns = ref<['start', 'pause', 'resume', 'restart']>([
       v-for="(item, index) in btns"
       :key="index"
     >
-      <button class="btn-primary btn" @click="emit(item)">
-        {{ item }}
+      <button
+        class="inline-block cursor-pointer rounded-10px border-solid px-4 py-1 text-white"
+        @click="emit(item)"
+      >
+        {{ item.toUpperCase() }}
       </button>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+button{
+  position: relative;
+  background: linear-gradient(243deg, #1b242b, #424f5e);
+  border: 1px solid #4e5566;
+}
+button:hover{
+  background: #424f5e;
+}
+button::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('/noise.png');
+  background-repeat: repeat;
+  opacity: .04;
+  pointer-events: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
 </style>
